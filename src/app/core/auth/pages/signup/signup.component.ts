050d7bda5b88fde34089c284953f05f386bf9232
coderdiaz <coderdiaz@gmail.com>
2024-09-12 15:20:50 -0600

    fix: minor visual improvements

--- src/app/core/auth/pages/signup/signup.component.ts
+++ src/app/core/auth/pages/signup/signup.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   imports: [
     FormsModule,
+    RouterLink,
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
