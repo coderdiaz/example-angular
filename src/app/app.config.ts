d33bda8465b4728385cc5e043d30a8a440ab91d2
coderdiaz <coderdiaz@gmail.com>
2024-09-13 09:06:37 -0600

    feat: configured interceptor in app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './core/auth/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
   ],
 };
