531096a270734812cb898025b6cb735e72121416
coderdiaz <coderdiaz@gmail.com>
2024-09-13 13:27:25 -0600

    feat: added updated auth component

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, distinctUntilChanged, map, Observable, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { User } from '../models/user.model';
 import { environment } from '../../../../environments/environment';
 import { JwtService } from './jwt.service';
@@ -11,6 +12,7 @@ import { JwtService } from './jwt.service';
 export class AuthService {
   private http = inject(HttpClient);
   private jwtService = inject(JwtService);
+  private router = inject(Router);
   private currentUserSubject = new BehaviorSubject<User | null>(null);
   public currentUser = this.currentUserSubject
     .asObservable()
@@ -43,8 +45,29 @@ export class AuthService {
       )
   }
 
+  logout() {
+    this.purgeAuth();
+    this.router.navigate(['/auth']);
+  }
+
   setAuth(user: User) {
     this.jwtService.saveToken(user.jwt);
     this.currentUserSubject.next(user);
+  }
+
+  purgeAuth() {
+    this.jwtService.destroyToken();
+    this.currentUserSubject.next(null);
+  }
+
+  getCurrentUser(): Observable<User> {
+    const jwt = this.jwtService.getToken();
+    return this.http.get<any>(`${environment.TICKETS_API_URL}/users/me`)
+      .pipe(
+        tap((response: any) => this.setAuth({
+          jwt,
+          user: response,
+        }))
+      )
   }
 }
