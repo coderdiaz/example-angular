38c0d1db12b8f80b2f2ab4d73c584f5e3cd4896f
coderdiaz <coderdiaz@gmail.com>
2024-09-13 13:26:46 -0600

    feat: added dashboard route

--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { authGuard } from '../../core/auth/guards/auth.guard';
 
 export const dashboardRoutes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    canMatch: [authGuard],
   }
 ];
--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
