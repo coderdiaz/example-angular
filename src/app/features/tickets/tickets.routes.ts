eb7567779f1f0fa1fcd6c58bc3d4a703951a3fab
coderdiaz <coderdiaz@gmail.com>
2024-09-13 11:24:19 -0600

    fix: updated guard implementation

--- src/app/features/tickets/tickets.routes.ts
+++ src/app/features/tickets/tickets.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { TicketListComponent } from './pages/ticket-list/ticket-list.component';
-import { inject } from '@angular/core';
-import { AuthService } from '../../core/auth/services/auth.service';
+import { authGuard } from '../../core/auth/guards/auth.guard';
 
 export const ticketRoutes: Routes = [
   {
     path: '',
     component: TicketListComponent,
-    canActivate: [
-      () => inject(AuthService).isAuthenticated,
-    ],
+    canMatch: [authGuard],
   },
 ];
