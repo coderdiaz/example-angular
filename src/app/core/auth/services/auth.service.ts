7c4b235c31280bdf08e9a24a39f367373348f24e
coderdiaz <coderdiaz@gmail.com>
2024-09-13 09:52:23 -0600

    feat: updated auth service

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { BehaviorSubject, distinctUntilChanged, map, Observable, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
 import { environment } from '../../../../environments/environment';
@@ -12,7 +12,11 @@ export class AuthService {
   private http = inject(HttpClient);
   private jwtService = inject(JwtService);
   private currentUserSubject = new BehaviorSubject<User | null>(null);
-  currentUser = this.currentUserSubject.asObservable();
+  public currentUser = this.currentUserSubject
+    .asObservable()
+    .pipe(distinctUntilChanged());
+
+  public isAuthenticated = this.currentUser.pipe(map((user) => !!user));
 
   constructor() {}
 
