1f60d0c0c0531f8b12931a7a99a15314f12c6867
coderdiaz <coderdiaz@gmail.com>
2024-09-13 13:26:29 -0600

    feat: added header component to ticket list

--- src/app/features/tickets/pages/ticket-list/ticket-list.component.ts
+++ src/app/features/tickets/pages/ticket-list/ticket-list.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-ticket-list',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+  ],
   templateUrl: './ticket-list.component.html',
   styleUrl: './ticket-list.component.css'
 })
